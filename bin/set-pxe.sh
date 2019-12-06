b92ea95d538384fa7fb62cb916e89d93ad7a9d32
Remus Lazar <rl@cron.eu>
2019-12-06 15:22:32 +0100

    Make set-pxe.sh XenServer version agnostic
    use the configured version from .env

--- bin/set-pxe.sh
+++ bin/set-pxe.sh
@@ -9,10 +9,9 @@
 
 ##
 # Configuration
-CITRIX_INSTALLER_NAME="CitrixHypervisor-${CITRIX_VERSION}-install"
 
 function usage {
-	echo "Usage: $0 <IP address or hostname> [ rescue | xenserver80-auto | off ]"
+	echo "Usage: $0 <IP address or hostname> [ rescue | xenserver-auto | off ]"
 	exit 1
 }
 
@@ -21,11 +20,14 @@ function error {
 	exit 2
 }
 
-if [ -z "$CITRIX_INSTALL" ]; then
-	error "ENV VAR CITRIX_INSTALL not set"
+if [ -z "$CITRIX_VERSION" ]; then
+	error "ENV VAR CITRIX_VERSION not set"
 fi
 
-xen_pxe_dir="xen-$CITRIX_INSTALL}"
+echo "Using XenServer Version: ${CITRIX_VERSION}"
+
+installer_name="CitrixHypervisor-${CITRIX_VERSION}-install"
+xen_pxe_dir="xen-installer-${CITRIX_VERSION}"
 
 host="$1"; shift
 action="$1"; shift
@@ -36,8 +38,8 @@ if [ -z "$action" ]; then
 	usage
 fi
 
-filename="${TFTP_ROOT}/pxelinux.cfg/$(gethostip -x $host)"
-host_ip=$(gethostip -n $host)
+filename="${TFTP_ROOT}/pxelinux.cfg/$(gethostip -x "$host")"
+host_ip=$(gethostip -n "$host")
 
 if [ -z "$filename" ]; then
 	error "Could not build the PXE config file name"
@@ -47,12 +49,12 @@ echo "Using PXE configuration file: $filename"
 
 case $action in
 off)
-	rm -f $filename
+	rm -f "$filename"
 	echo "PXE configuration removed for ip ${host_ip}"
 	;;
 
 rescue)
-	cat <<EOF > $filename
+	cat <<EOF > "$filename"
 default $action
 label $action
         kernel archlinux/vmlinuz_i686
@@ -61,12 +63,13 @@ EOF
 	echo "PXE action set to $action for ip ${host_ip}"
 	;;
 
-xenserver80-auto)
-	cat <<EOF > $filename
+xenserver-auto)
+	cat <<EOF > "$filename"
 default $action
 label $action
 	kernel mboot.c32
-	append ${xen_pxe_dir}/xen.gz dom0_max_vcpus=1-16 dom0_mem=max:8192M com1=115200,8n1 console=com1,vga --- ${xen_pxe_dir}/vmlinuz xencons=hvc console=hvc0 console=tty0 answerfile=${HTTPHOST_BASEURL}/${CITRIX_INSTALLER_NAME}-answerfile install --- ${xen_pxe_dir}/install.img
+	append ${xen_pxe_dir}/xen.gz dom0_max_vcpus=1-16 dom0_mem=max:8192M com1=115200,8n1 console=com1,vga --- ${xen_pxe_dir}/vmlinuz xencons=hvc console=hvc0 console=tty0 answerfile=${HTTPHOST_BASEURL}/${installer_name}="CitrixHypervisor-${CITRIX_VERSION}-install"
+}-answerfile install --- ${xen_pxe_dir}/install.img
 EOF
 	echo "PXE action set to $action for ip ${host_ip}"
 	;;
